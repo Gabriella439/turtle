e6b6e73bcd048ceb64f94a2c227efdd3fdd3ecbf
Pierre Radermecker <pierrer@pi3r.be>
2015-02-26 11:20:35 +0100

    fix fp doc: complete link to FilePath

--- src/Turtle/Format.hs
+++ src/Turtle/Format.hs
@@ -180,7 +180,7 @@ g = makeFormat (\n -> pack (showGFloat (Just 6) n ""))
 s :: Format r (Text -> r)
 s = makeFormat id
 
-{-| `Format` a `Filesystem.Path.CurrentOS` into `Text`
+{-| `Format` a `Filesystem.Path.CurrentOS.FilePath` into `Text`
 -}
 fp :: Format r (FilePath -> r)
 fp = makeFormat (\fpath -> either id id (toText fpath))
