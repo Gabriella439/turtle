2061d509031fc488cf0511ac61823ea91e1a3b1b
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-01-22 19:00:43 -0800

    Add nix derivation to build `turtle`

---
+++ release.nix
@@ -0,0 +1,32 @@
+# You can build this repository using Nix by running:
+#
+#     $ nix-build -A turtle release.nix
+#
+# You can also open up this repository inside of a Nix shell by running:
+#
+#     $ nix-shell -A turtle.env release.nix
+#
+# ... and then Nix will supply the correct Haskell development environment for
+# you
+let
+  config = {
+    packageOverrides = pkgs: {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: {
+          optparse-applicative =
+            pkgs.haskell.lib.dontCheck
+              (haskellPackagesNew.callPackage ./optparse-applicative.nix { });
+
+          turtle =
+            haskellPackagesNew.callPackage ./default.nix { };
+        };
+      };
+    };
+  };
+
+  pkgs =
+    import <nixpkgs> { inherit config; };
+
+in
+  { turtle = pkgs.haskellPackages.turtle;
+  }
