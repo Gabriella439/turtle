ebac510c6726e9617d436dc99ba4505ddbf911cf
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-03-15 08:27:00 -0700

    Version 1.5.5 → 1.5.6 (#293)

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@
 }:
 mkDerivation {
   pname = "turtle";
-  version = "1.5.5";
+  version = "1.5.6";
   src = ./.;
   libraryHaskellDepends = [
     ansi-wl-pprint async base bytestring clock containers directory
