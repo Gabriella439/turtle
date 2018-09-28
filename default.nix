6ba3f47098f6e7c0f9a7163658f5fd7e9509bb5d
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-09-28 13:59:07 -0700

    Use updated `fetchNixpkgs.nix` (#327)

--- default.nix
+++ default.nix
@@ -4,9 +4,7 @@ let
   nixpkgs = fetchNixpkgs {
     rev = "804060ff9a79ceb0925fe9ef79ddbf564a225d47";
 
-    sha256 = "01pb6p07xawi60kshsxxq1bzn8a0y4s5jjqvhkwps4f5xjmmwav3";
-
-    outputSha256 = "0ga345hgw6v2kzyhvf5kw96hf60mx5pbd9c4qj5q4nan4lr7nkxn";
+    sha256 = "0ga345hgw6v2kzyhvf5kw96hf60mx5pbd9c4qj5q4nan4lr7nkxn";
   };
 
   readDirectory = import ./nix/readDirectory.nix;
